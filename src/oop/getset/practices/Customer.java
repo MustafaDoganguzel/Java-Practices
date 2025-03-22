a38e80d7f70ff05d58ceacbff5661c106d4ea18f
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-03-23 02:03:20 +0300

    Implement Customer class with constructors and getter methods

---
+++ src/oop/getset/practices/Customer.java
@@ -0,0 +1,52 @@
+package oop.getset.practices;
+
+public class Customer {
+
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public Customer() {
+        this("Nobody", "nobody@nowhere.com");
+    }
+
+    public Customer(String name, String email) {
+        this(name, 1000, email);
+    }
+
+    public Customer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public static void main(String[] args) {
+        Customer customer = new Customer("Tim", 1000,
+                "tim@email.com");
+        System.out.println(customer.getName());
+        System.out.println(customer.getCreditLimit());
+        System.out.println(customer.getEmail());
+
+        Customer secondCustomer = new Customer();
+        System.out.println(secondCustomer.getName());
+        System.out.println(secondCustomer.getCreditLimit());
+        System.out.println(secondCustomer.getEmail());
+
+        Customer thirdCustomer = new Customer("Joe", "joe@email.com");
+        System.out.println(thirdCustomer.getName());
+        System.out.println(thirdCustomer.getCreditLimit());
+        System.out.println(thirdCustomer.getEmail());
+    }
+}
